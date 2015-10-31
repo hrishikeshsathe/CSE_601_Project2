3accee4b56c5d12eb72b6f2ec7cf702bc8699e03
Hrishikesh Sathe <hrishikesh.sathe90@gmail.com>
2015-10-31 14:40:20 -0400

    Parsed input data to get Gene POJOs

---
+++ src/com/dm/clustering/Main.java
@@ -0,0 +1,18 @@
+package com.dm.clustering;
+
+import com.dm.clustering.data.pojo.Gene;
+import com.dm.clustering.data.reader.InputParser;
+
+import java.util.ArrayList;
+
+public class Main {
+    static final String FILE_ONE_PATH = "D:\\Courses\\DM\\Project 2\\Data\\iyer.txt";
+
+    public static void main(String args[]) {
+        InputParser inputParser = new InputParser(FILE_ONE_PATH);
+        ArrayList<Gene> genesList = inputParser.parseData();
+        if(genesList.size() > 0) {
+
+        }
+    }
+}
