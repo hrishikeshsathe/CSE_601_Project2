29a63a2e0d70c8af12b6642f8bd5325552362205
Hrishikesh Sathe <hrishikesh.sathe90@gmail.com>
2015-11-01 19:27:43 -0500

    Select inital K centroids using Kmeans++

---
+++ src/com/dm/clustering/utility/Utility.java
@@ -0,0 +1,23 @@
+package com.dm.clustering.utility;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Hrishikesh on 11/1/2015.
+ */
+public class Utility {
+
+    /**
+     * Calculate euclidean distance between two lists of points
+     * @param list1 points in list1
+     * @param list2 points in list2
+     * @return distance
+     */
+    public static double calculateEuclideanDistance(ArrayList<Double> list1, ArrayList<Double> list2) {
+        double sum = 0.0;
+        for(int i = 0; i < list1.size(); i++) {
+            sum += Math.pow(list1.get(i) - list2.get(i), 2);
+        }
+        return Math.sqrt(sum);
+    }
+}
