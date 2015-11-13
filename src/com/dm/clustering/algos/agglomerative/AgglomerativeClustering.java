f781951a0df7082357646986c8ab1e91c5ca52d8
Samved Divekar <divekar.samved@gmail.com>
2015-11-13 18:52:47 -0500

    complete link clustering method added

--- src/com/dm/clustering/algos/agglomerative/AgglomerativeClustering.java
+++ src/com/dm/clustering/algos/agglomerative/AgglomerativeClustering.java
@@ -66,7 +66,7 @@ public class AgglomerativeClustering {
     }
 
 
-    public static double getDistBetweenClusters(AgglomerativeCluster cluster1, AgglomerativeCluster cluster2) {
+    public static double getDistBetweenClustersUsingSingleLink(AgglomerativeCluster cluster1, AgglomerativeCluster cluster2) {
         List<Gene> clusterOneGenes = cluster1.getGenes();
         List<Gene> clusterTwoGenes = cluster2.getGenes();
         double distBetweenClusters = Double.MAX_VALUE;
@@ -80,19 +80,33 @@ public class AgglomerativeClustering {
         return distBetweenClusters;
     }
 
+    public static double getDistBetweenClustersUsingCompleteLink(AgglomerativeCluster cluster1, AgglomerativeCluster cluster2) {
+        List<Gene> clusterOneGenes = cluster1.getGenes();
+        List<Gene> clusterTwoGenes = cluster2.getGenes();
+        double distBetweenClusters = Double.MIN_VALUE;
+        for (Gene g1 : clusterOneGenes) {
+            for (Gene g2 : clusterTwoGenes) {
+                double distBetweenGenes = Utility.calculateEuclideanDistance(g1.getGeneExpValues(), g2.getGeneExpValues());
+                if (distBetweenClusters < distBetweenGenes )
+                    distBetweenClusters = distBetweenGenes;
+            }
+        }
+        return distBetweenClusters;
+    }
+
     public static ClusterPair findClosestClusters() {
 
         AgglomerativeCluster first = null;
         AgglomerativeCluster second = null;
         double minDist = Double.MAX_VALUE;
-        int sampleCount = 0;
         for (int i = 0; i < currentClusters.size(); i++) {
             AgglomerativeCluster temp1 = currentClusters.get(i);
             for (int j = 0; j < currentClusters.size(); j++) {
                 if (i == j)
                     continue;
                 AgglomerativeCluster temp2 = currentClusters.get(j);
-                double distBetweenClusters = getDistBetweenClusters(temp1, temp2);
+                //double distBetweenClusters = getDistBetweenClustersUsingSingleLink(temp1, temp2);
+                double distBetweenClusters = getDistBetweenClustersUsingCompleteLink(temp1, temp2);
                 if (distBetweenClusters < minDist) {
                     first = temp1;
                     second = temp2;
@@ -117,7 +131,7 @@ public class AgglomerativeClustering {
     public static void generateInputForDendrogram()
     {
         File f = new File("HACResults.txt");
-        String singleRecord = "";
+        String singleRecord;
         try {
             FileWriter fw = new FileWriter(f);
             BufferedWriter bw = new BufferedWriter(fw);
